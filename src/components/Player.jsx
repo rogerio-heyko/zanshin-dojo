4d3b99e01c6a0c38fabf6f14779059f87889c321
Rogério <rogerio@powerdev.com>
2026-03-29 10:02:17 -0300

    feat: add advanced combat sprites (idle, punch, kick)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,19 +6,27 @@ const Player = () => {
 
     const getPlayerClass = () => {
         switch (playerState) {
+            case 'IDLE': return 'player-idle';
             case 'ATTACK_L_PERFECT': return 'player-strike-left';
             case 'ATTACK_R_PERFECT': return 'player-strike-right';
             case 'MISS': return 'player-vulnerable';
             case 'HIT': return 'player-damaged';
-            case 'DEFEATED': return 'player-defeated';
+            case 'DEFEATED': return 'player-damaged';
             default: return 'player-idle';
         }
     };
 
+    let imageSrc = '/player_idle.png';
+    if (playerState === 'ATTACK_L_PERFECT') {
+        imageSrc = '/player_punch.png';
+    } else if (playerState === 'ATTACK_R_PERFECT') {
+        imageSrc = '/player_kick.png';
+    }
+
     return (
         <div className={`player-container ${getPlayerClass()}`}>
             <img
-                src="/player.png"
+                src={imageSrc}
                 alt="Player Kimono"
                 className="player-sprite"
             />
